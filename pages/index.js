01940f1a1dddf4c8647cbcfff80c32e11ec57fbd
sanumuhammedc <sanumuhammed.c@gmail.com>
2022-10-27 11:51:17 +0530

    installed bootstrap and material ui

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 // import Image from 'next/image'
 import Head from "next/head";
+
 import Carousel from "../Components/Carousel";
 import Footer from "../Components/Footer";
 import Main from "../Components/Main";
 import Navbar from "../Components/Navbar";
 import styles from "../styles/Home.module.css";
-import Button from '@mui/material/Button';
+
 
 export default function Home() {
   return (
